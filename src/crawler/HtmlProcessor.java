10d14fdce3333b8f3149186a9f033c8913bb985b
josuerocha <josuerocha@me.com>
2017-09-29 16:04:10 -0300

    [MAJOR] Adding HtmlProcessor class

--- src/crawler/HtmlProcessor.java
+++ src/crawler/HtmlProcessor.java
@@ -5,6 +5,8 @@
  */
 package crawler;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.htmlcleaner.CleanerProperties;
 import org.htmlcleaner.HtmlCleaner;
 import org.htmlcleaner.TagNode;
@@ -14,15 +16,35 @@ import org.htmlcleaner.TagNode;
  * @author jr
  */
 public class HtmlProcessor {
+    
+        private static HtmlProcessor htmlProcessor = new HtmlProcessor();
         private static HtmlCleaner htmlCleaner = new HtmlCleaner();
+        
+        public static HtmlProcessor getInstance(){
+            if(htmlProcessor == null){
+                htmlProcessor = new HtmlProcessor();
+            }
+            
+            return htmlProcessor;
+        }
         
         public HtmlProcessor(){
             CleanerProperties props = htmlCleaner.getProperties();
         }
         
-        public String cleanHtml(String pageContent){
-            TagNode cleanedPage = htmlCleaner.clean(pageContent);
-            cleanedPage.
+        public List<String> extractLinks(String pageContent){
+            TagNode rootNode = htmlCleaner.clean(pageContent);
             
+            List<String> links = new ArrayList<String> ();
+            TagNode linkElements[] = rootNode.getElementsByName("href",true);
+            
+            for ( TagNode node : linkElements){
+                String link = node.getAttributeByName ("href");
+                if(link != null && link.length() > 0){
+                    links.add(link);
+                }
+            }
+            
+            return links;
         }
 }
