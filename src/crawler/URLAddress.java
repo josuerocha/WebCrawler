0270192b01c20d790ac930288990649e98ef75f4
josuerocha <josuerocha@me.com>
2017-09-30 23:34:44 -0300

    [MAJOR] Moving all address functions to URLAddress class.

--- src/crawler/URLAddress.java
+++ src/crawler/URLAddress.java
@@ -6,83 +6,103 @@ import java.net.URL;
 import java.net.UnknownHostException;
 
 public class URLAddress {
-	private URL address;
-	private int depth;
-        
-	public URLAddress(String url,int depth) throws MalformedURLException
-	{
-		
-		this.address =  new URL(formatURL(url));
-		this.depth = depth;
-	}
-	public static String formatURL(String url)
-	{
-		if(!url.matches("[a-zA-Z]+://.*"))
-		{
-			url = "http://"+url;
-		}
-		
-		return url;
-	}
-	public static String getDomain(String address) throws MalformedURLException
-	{
-		return new URL(formatURL(address)).getHost();
-	}
-	public String getProtocol()
-	{
-		return this.address.getProtocol();
-	}
-	public String getDomain()
-	{
-	
-		return address.getHost();
-	}
-	public String getAddress() {
-		return address.getProtocol()+"://"+address.getHost()+address.getFile();
-	}
-	public int getDepth() {
-		return depth;
-	}
 
-	public String getPath() {
-		// TODO Auto-generated method stub
-		//return address.getPath().length()==0?"/":"";
-                return address.getPath();
-	}
-        
-        public URL getUrlRobotsTxt() throws MalformedURLException{
-            
-            return new URL(address.getProtocol() + "://" + address.getHost() + "/robots.txt");
+    private URL address;
+    private int depth;
+
+    public URLAddress(String url, int depth) throws MalformedURLException {
+
+        this.address = new URL(formatURL(url));
+        this.depth = depth;
+    }
+
+    public static String formatURL(String url) {
+        if (!url.matches("[a-zA-Z]+://.*")) {
+            url = "http://" + url;
         }
-	
-	public URL getUrlObj(){
-            return this.address;
+
+        return url;
+    }
+
+    public static String getDomain(String address) throws MalformedURLException {
+        return new URL(formatURL(address)).getHost();
+    }
+
+    /**
+     * Verifica se uma url é relativa ou absoluta
+     *
+     * @param urlString
+     * @return
+     */
+    public static boolean isAbsoluteURL(String urlString) {
+        boolean result = false;
+        try {
+            URL url = new URL(urlString);
+            String protocol = url.getProtocol();
+            if (protocol != null && protocol.trim().length() > 0) {
+                result = true;
+            }
+        } catch (MalformedURLException e) {
+            return false;
         }
-	
-	public static void main(String[] args) throws MalformedURLException, UnknownHostException
-	{
-		URLAddress url1 = new URLAddress("http://www.terra.com.br/oioi/lala", 0);
-		System.out.println("Dominio: "+url1.getDomain());
-		System.out.println("Caminho: "+url1.getPath());
-		System.out.println("Completo: "+url1.getAddress());
-		
-		URLAddress url2 = new URLAddress("esportes.terra.com.br/kaak/oioi", 0);
-		System.out.println("Dominio: "+url2.getDomain());
-		System.out.println("Caminho: "+url2.getPath());
-		System.out.println("Completo: "+url2.getAddress());
-		
-		URL url = new URL("http://www.terra.com.br");
-		System.out.println(url.getHost());
-		InetAddress address = InetAddress.getByName(url.getHost());
-		System.out.println(address.getHostAddress());
-		
-		url = new URL("http://esportes.terra.com.br");
-		System.out.println(url.getHost());
-		address = InetAddress.getByName(url.getHost());
-		System.out.println(address.getHostAddress());
-	}
-	public String toString()
-	{
-		return address.toString();
-	}
+        return result;
+    }
+
+    public String getProtocol() {
+        return this.address.getProtocol();
+    }
+
+    public String getDomain() {
+
+        return address.getHost();
+    }
+
+    public String getAddress() {
+        return address.getProtocol() + "://" + address.getHost() + address.getFile();
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public String getPath() {
+        // TODO Auto-generated method stub
+        //return address.getPath().length()==0?"/":"";
+        return address.getPath();
+    }
+
+    public URL getUrlRobotsTxt() throws MalformedURLException {
+
+        return new URL(address.getProtocol() + "://" + address.getHost() + "/robots.txt");
+    }
+
+    public URL getUrlObj() {
+        return this.address;
+    }
+
+    public static void main(String[] args) throws MalformedURLException, UnknownHostException {
+        URLAddress url1 = new URLAddress("http://www.terra.com.br/oioi/lala", 0);
+        System.out.println("Dominio: " + url1.getDomain());
+        System.out.println("Caminho: " + url1.getPath());
+        System.out.println("Completo: " + url1.getAddress());
+
+        URLAddress url2 = new URLAddress("esportes.terra.com.br/kaak/oioi", 0);
+        System.out.println("Dominio: " + url2.getDomain());
+        System.out.println("Caminho: " + url2.getPath());
+        System.out.println("Completo: " + url2.getAddress());
+
+        URL url = new URL("http://www.terra.com.br");
+        System.out.println(url.getHost());
+        InetAddress address = InetAddress.getByName(url.getHost());
+        System.out.println(address.getHostAddress());
+
+        url = new URL("http://esportes.terra.com.br");
+        System.out.println(url.getHost());
+        address = InetAddress.getByName(url.getHost());
+        System.out.println(address.getHostAddress());
+    }
+
+    public String toString() {
+        return address.toString();
+    }
 }
