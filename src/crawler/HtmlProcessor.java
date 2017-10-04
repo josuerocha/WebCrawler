171a2749c6092a7417b366b162c0c3f1dcafff35
josuerocha <josuerocha@me.com>
2017-10-04 17:53:09 -0300

    [MAJOR] Implemented JVM memory max 7000MB.

--- src/crawler/HtmlProcessor.java
+++ src/crawler/HtmlProcessor.java
@@ -5,6 +5,7 @@
  */
 package crawler;
 
+import com.trigonic.jrobotx.Constants;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -24,44 +25,93 @@ import org.htmlcleaner.TagNode;
  * @author jr
  */
 public class HtmlProcessor {
-    
-        private static HtmlProcessor htmlProcessor;
-        private static HtmlCleaner htmlCleaner;
-        private static String jsPattern = new String("(.)*javascript(.)*");
-        
-        public static HtmlProcessor getInstance(){
-            if(htmlProcessor == null){
-                htmlProcessor = new HtmlProcessor();
-            }
-            
-            return htmlProcessor;
-        }
-        
-        private HtmlProcessor(){
-            htmlCleaner = new HtmlCleaner();
-            CleanerProperties props = htmlCleaner.getProperties();
-            props.setAllowHtmlInsideAttributes(true);
-            props.setAllowMultiWordAttributes(true);
-            props.setRecognizeUnicodeChars(true);
-            props.setOmitComments(true);
-        }
-        
-        public List<String> extractLinks(String pageContent) {
-            
-            TagNode rootNode = htmlCleaner.clean(pageContent);
-            
-            List<String> links = new ArrayList<> ();
-            TagNode linkElements[] = rootNode.getElementsByName("a",true);
-            
-            for ( TagNode node : linkElements){
 
-                String link = node.getAttributeByName("href");
-                if(link != null && link.length() > 0 && !link.matches(jsPattern)){
-                    links.add(link);
-                }
-            }
-            
-            return links;
-           
+    private static HtmlProcessor htmlProcessor;
+    private static HtmlCleaner htmlCleaner;
+    private static String jsPattern = new String("(.)*javascript(.)*");
+
+    public static HtmlProcessor getInstance() {
+        if (htmlProcessor == null) {
+            htmlProcessor = new HtmlProcessor();
         }
+
+        return htmlProcessor;
+    }
+
+    private HtmlProcessor() {
+        htmlCleaner = new HtmlCleaner();
+        CleanerProperties props = htmlCleaner.getProperties();
+        props.setAllowHtmlInsideAttributes(true);
+        props.setAllowMultiWordAttributes(true);
+        props.setRecognizeUnicodeChars(true);
+        props.setOmitComments(true);
+    }
+
+    public List<String> extractLinks(String pageContent) {
+
+        TagNode rootNode = htmlCleaner.clean(pageContent);
+
+        List<String> links = new ArrayList<>();
+        TagNode linkElements[] = rootNode.getElementsByName("a", true);
+
+        for (TagNode node : linkElements) {
+
+            String link = node.getAttributeByName("href");
+            if (link != null && link.length() > 0 && !link.matches(jsPattern)) {
+                links.add(link);
+            }
+        }
+
+        return links;
+
+    }
+
+    public boolean[] allowsIndexing(String pageContent) {
+
+        boolean permission[] = {true, true};
+
+        TagNode rootNode = htmlCleaner.clean(pageContent);
+
+        TagNode metaElements[] = rootNode.getElementsByName("meta", true);
+
+        for (TagNode node : metaElements) {
+            String name = node.getAttributeByName("name");
+            if (name != null && name.equalsIgnoreCase("robots")) {
+
+                String content = node.getAttributeByName("content");
+                String attributes[] = content.split(",");
+
+                switch (attributes.length) {
+                    case 1:
+                        if (attributes[0].equalsIgnoreCase(Constants.NOINDEX)) {
+                            permission[0] = false;
+                        }
+                        System.out.print("ATTRIBUTES: " + attributes[0]);
+
+                        break;
+
+                    case 2:
+                        if (attributes[0].equalsIgnoreCase(Constants.NOINDEX)) {
+                            permission[0] = false;
+                        }
+
+                        if (attributes[1].equalsIgnoreCase(Constants.NOFOLLOW)) {
+                            permission[1] = false;
+                        }
+                        System.out.print("ATTRIBUTES: " + attributes[0] + " " + attributes[1]);
+                        break;
+                }
+                
+                System.out.print(" PERMISSIONS " + permission[0] + " " + permission[1]);
+                
+                if(permission[0] == false || permission[1] == false){
+                    System.exit(1);
+                }
+                
+            }
+        }
+
+        return permission;
+
+    }
 }
