c0ddaf8160f3a6498b487748573e848a3cc61801
josuerocha <josuerocha@me.com>
2017-10-09 01:05:25 -0300

    [MAJOR] FINAL RELEASE

--- test/chart1/chart1.m
+++ test/chart1/chart1.m
@@ -3,18 +3,23 @@
 threads = [10 30 50 70 90 100];
 %Average
 avgTime = [100.5 53.25 54.5 48.5 54.5 68.25];
+avgTime = 500 ./ avgTime;
 
 %Test 1
 test1Time = [92 58 50 50 51 58];
+test1Time = 500 ./ test1Time;
 
 %Test 2
 test2Time = [107 54 57 46 58 50];
+test2Time = 500 ./ test2Time;
 
 %Test 3
 test3Time = [98 53 54 54 50 56];
+test3Time = 500 ./ test3Time;
 
 %Test 4
 test4Time = [105 48 57 44 59 57];
+test4Time = 500 ./ test4Time;
 
 %% Plotting
 plot(threads,test1Time);
@@ -29,6 +34,6 @@ plot(threads,avgTime,'LineWidth',3);
 
 legend('Teste 1','Teste 2','Teste 3','Teste 4','Média aritmética');
 xlabel('Número de threads');
-ylabel('Tempo de execução (segundos)');
+ylabel('Páginas por segundo');
 grid
 title('Tempo de execução relacionado à quantidade de threads');
--- test/chart2/chart2.m
+++ test/chart2/chart2.m
@@ -2,19 +2,24 @@
 %% Defining values
 threads = [10 30 50 70 90 100];
 %Average
-avgTime = [100.5 53.25 54.5 48.5 54.5 68.25];
+avgTime = [118.5 90.75 78 77.52 76 67.5];
+avgTime = 500 ./ avgTime;
 
 %Test 1
-test1Time = [92 58 50 50 51 58];
+test1Time = [107 87 70 58 82 62];
+test1Time = 500 ./ test1Time;
 
 %Test 2
-test2Time = [107 54 57 46 58 50];
+test2Time = [121 91 84 80 72 62];
+test2Time = 500 ./ test2Time;
 
 %Test 3
-test3Time = [98 53 54 54 50 56];
+test3Time = [109 102 82 93 76 70];
+test3Time = 500 ./ test3Time;
 
 %Test 4
-test4Time = [105 48 57 44 59 57];
+test4Time = [137 83 76 78 74 76];
+test4Time = 500 ./ test4Time;
 
 %% Plotting
 plot(threads,test1Time);
@@ -29,6 +34,6 @@ plot(threads,avgTime,'LineWidth',3);
 
 legend('Teste 1','Teste 2','Teste 3','Teste 4','Média aritmética');
 xlabel('Número de threads');
-ylabel('Tempo de execução (segundos)');
+ylabel('Páginas por segundo');
 grid
 title('Tempo de execução relacionado à quantidade de threads');
