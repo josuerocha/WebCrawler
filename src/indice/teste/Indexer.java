491e956bffdf6fca24cdb34ee48eca0b359a91bf
josuerocha <josuerocha@me.com>
2017-10-18 17:39:52 -0200

    [MAJOR] DocId extraction implemented. .

---
+++ src/indice/teste/Indexer.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package indice.teste;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author jr
+ */
+public class Indexer {
+    
+    private Pattern docIdPattern = Pattern.compile("([0-9])*");
+    private File rootdir;
+    private String dirpath;
+    
+    public Indexer(String dirpath){
+        this.dirpath = dirpath;
+        rootdir = new File(dirpath);
+        
+    }
+    
+    public void getFiles(){
+        
+        BufferedReader bufferedReader;
+        
+        for(File subdir : rootdir.listFiles(File::isDirectory)){
+            for(File htmlFile : subdir.listFiles()){
+                
+                try{
+                    bufferedReader = new BufferedReader(new FileReader(htmlFile));
+                    String content;
+                    while(( content = bufferedReader.readLine()) != null){
+                        
+                    }
+                    
+                    System.out.println(htmlFile.getName());
+                    Matcher matcher = docIdPattern.matcher(htmlFile.getName());
+                    matcher.find();
+                    int docId = Integer.parseInt(matcher.group());
+                    indexDocument(content,docId);
+                    
+                }catch(Exception ex){
+                    ex.printStackTrace();
+                }
+            
+            }
+            
+        }
+    }
+    
+    
+    public void indexDocument(String content,int docId){
+        
+    }
+    
+    public static void main(String[] args) {
+        String wikipath = "wikiSample";
+        Indexer indexer = new Indexer(wikipath);
+        indexer.getFiles();
+        
+    }
+   
+    
+}
+
