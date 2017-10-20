983889f8ad6339fcd881c7160dce8dedfa121d20
josuerocha <josuerocha@me.com>
2017-10-20 21:54:24 -0200

    [MAJOR] Index document implemented. .

--- src/indice/teste/Indexer.java
+++ src/indice/teste/Indexer.java
@@ -4,10 +4,13 @@
  * and open the template in the editor.
  */
 package indice.teste;
+
 import util.StringUtil;
 import util.ArquivoUtil;
 import java.io.BufferedReader;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,74 +19,85 @@ import java.util.regex.Pattern;
  * @author jr e Tulio Fonseca
  */
 public class Indexer {
-    
+
     private Pattern docIdPattern = Pattern.compile("([0-9])*");
     private File rootdir;
     private String dirpath;
+
     /**
-     *  
      *
-     * @param 
-     * @return
+     *
+     * @param @return
      */
-    public Indexer(String dirpath){
+    public Indexer(String dirpath) {
         this.dirpath = dirpath;
         rootdir = new File(dirpath);
-        
-    }
-    /**
-     *  
-     *
-     * @param 
-     * @return
-     */
-    public void getFiles(){
 
-        for(File subdir : rootdir.listFiles(File::isDirectory)){
-            for(File htmlFile : subdir.listFiles()){
-               
-                try{
+    }
+
+    /**
+     *
+     *
+     * @param @return
+     */
+    public void getFiles() {
+
+        for (File subdir : rootdir.listFiles(File::isDirectory)) {
+            for (File htmlFile : subdir.listFiles()) {
+
+                try {
                     String content = ArquivoUtil.leTexto(htmlFile);
-                    
+
                     System.out.println(htmlFile.getName());
                     Matcher matcher = docIdPattern.matcher(htmlFile.getName());
                     matcher.find();
                     int docId = Integer.parseInt(matcher.group());
-                    indexDocument(content,docId);
-                    
-                }catch(Exception ex){
+                    indexDocument(content, docId);
+
+                } catch (Exception ex) {
                     ex.printStackTrace();
                 }
-            
+
             }
-            
+
         }
     }
-    
+
     /**
-     *  
      *
-     * @param 
-     * @return
+     *
+     * @param @return
      */
-    public void indexDocument(String content,int docId){
+    public void indexDocument(String content, int docId) {
         content = StringUtil.replaceAcento(content);
         content = content.toLowerCase();
-        
-        String[] terms = content.split("");
-        
-        for(String term : terms){
-            
-        }
+
     }
-    
+
+    public Map<String, Integer> getTermFrequency(String content) {
+        Map<String, Integer> termFrequency = new HashMap<>();
+
+        String[] terms = content.split("[\\D\\W]");
+
+        for (String term : terms) {
+            if (!StringUtil.isStopWord(term)) {
+                if (termFrequency.containsKey(term)) {
+                    Integer frequency = termFrequency.get(term) + 1;
+                    termFrequency.put(term, frequency);
+                } else {
+                    termFrequency.put(term, 1);
+                }
+            }
+        }
+        
+        return termFrequency;
+    }
+
     public static void main(String[] args) {
         String wikipath = "wikiSample";
         Indexer indexer = new Indexer(wikipath);
         indexer.getFiles();
-        
-    }
-   
-    
-}
 
+    }
+
+}
