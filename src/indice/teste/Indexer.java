c27e7e894d6e1d9f53bd21395b2ee2a5a9cd09fe
Caio Silva Gonçalves <caiosgon3@gmail.com>
2017-10-20 22:41:18 -0200

    IndexDocument Implemented

--- src/indice/teste/Indexer.java
+++ src/indice/teste/Indexer.java
@@ -5,6 +5,8 @@
  */
 package indice.teste;
 
+import indice.estrutura.Indice;
+import indice.estrutura.IndiceLight;
 import util.StringUtil;
 import util.ArquivoUtil;
 import ptstemmer.Stemmer;
@@ -15,9 +17,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.clapper.util.html.HTMLUtil;
 import ptstemmer.exceptions.PTStemmerException;
 import ptstemmer.implementations.OrengoStemmer;
-
 
 public class Indexer {
 
@@ -25,27 +27,32 @@ public class Indexer {
     private File rootdir; // Diretorio raiz
     private String dirpath; // String que recebe o caminho do diretorio dos arquivos
     private Stemmer ptStemmer;
+    private Indice indice;
+
     /**
-     *  Construtor da classe Indexer, ele recebe o caminho do diretorio dos arquivos e 
-     *  inicia o diretorio raiz dos arquivos.
+     * Construtor da classe Indexer, ele recebe o caminho do diretorio dos
+     * arquivos e inicia o diretorio raiz dos arquivos.
      *
-     * @param 
+     * @param
      * @return
      */
     public Indexer(String dirpath) {
-        this.dirpath = dirpath;
+        this.dirpath = dirpath;        
         rootdir = new File(dirpath);
+        
         try {
             ptStemmer = new OrengoStemmer();
+            indice = new IndiceLight(15000);
         } catch (PTStemmerException ex) {
             Logger.getLogger(Indexer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    /** 
-     * Função responsavel por ler todos os arquivos das pastas no diretorio e indexa-los.
+    /**
+     * Função responsavel por ler todos os arquivos das pastas no diretorio e
+     * indexa-los.
      *
-     * @param 
+     * @param
      * @return
      */
     public void getFiles() {
@@ -55,12 +62,12 @@ public class Indexer {
 
                 try {
                     String content = ArquivoUtil.leTexto(htmlFile);
-
+                    
                     System.out.println(htmlFile.getName());
                     Matcher matcher = docIdPattern.matcher(htmlFile.getName());
                     matcher.find();
-                    int docId = Integer.parseInt(matcher.group());
-                    indexDocument(content, docId);
+                    int docId = Integer.parseInt(matcher.group());                    
+                    indexDocument(HTMLUtil.textFromHTML(content), docId);
 
                 } catch (Exception ex) {
                     ex.printStackTrace();
@@ -72,21 +79,25 @@ public class Indexer {
     }
 
     /**
-     *  Função que recebe um texto (content) e executa as funções de retirar os acentos e deixa-las no 
-     *  diminutivo, ignorar as stopwords e stemmer, alem de calcular a frequencia de 
-     *  cada termo ja tratado no documento.
+     * Função que recebe um texto (content) e executa as funções de retirar os
+     * acentos e deixa-las no diminutivo, ignorar as stopwords e stemmer, alem
+     * de calcular a frequencia de cada termo ja tratado no documento.
      *
      * @param content,docId
      * @return
      */
     public void indexDocument(String content, int docId) {
+        // Pre-processamento do conteudo das paginas
         content = StringUtil.replaceAcento(content);
         content = content.toLowerCase();
         
+        // Obtendo Map de ocorrencias de termos no documento
         Map<String, Integer> termFrequency = getTermFrequency(content);
         
-        
-        
+        for (String term : termFrequency.keySet()) {           
+            indice.index(term, docId, termFrequency.get(term));
+        }
+
     }
 
     public Map<String, Integer> getTermFrequency(String content) {
@@ -96,6 +107,7 @@ public class Indexer {
 
         for (String term : terms) {
             
+            if(termIsEmpty(term)) continue;
             if (!StringUtil.isStopWord(term)) {
                 term = ptStemmer.getWordStem(term);
                 if (termFrequency.containsKey(term)) {
@@ -106,8 +118,13 @@ public class Indexer {
                 }
             }
         }
-        
+            
+
         return termFrequency;
+    }
+    
+    public boolean termIsEmpty(String term){
+        return term.length()==0;        
     }
 
     public static void main(String[] args) {
